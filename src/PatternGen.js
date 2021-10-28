17385570e7ff0bd7e9a44dedebab364c89c790d0
Oran C <oranbusiness@gmail.com>
2021-10-27 20:58:34 -0700

    [add] prattern

--- src/PatternGen.js
+++ src/PatternGen.js
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
 import RandExp from "randexp"
 import h from "./helper"
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
 const patternExamples = [
     { title: "Clear", example: "" },
@@ -17,20 +26,59 @@ const patternExamples = [
 ]
 
 
-const PatternGen = ({editor}) => {
+const PatternGen = ({ editor }) => {
     const [patterinput, setPatternInput] = useState("")
     const [patterOutput, setPatternOutput] = useState("")
-    const geneartePattern = ()=>{
+    const geneartePattern = () => {
         let regex_from_string = h.string_to_regexp(patterinput)
         let temp_pattern = new RandExp(regex_from_string).gen()
         setPatternOutput(temp_pattern)
         return temp_pattern
     }
-    
+    const [alignment, setAlignment] = React.useState('web');
+
+    const handleChange = (event, newAlignment) => {
+      setAlignment(newAlignment);
+    };
     return (
         <div>
 
-            <label htmlFor="pattern-input">Pattern</label>
+            <Stack spacing={2} direction="column">
+
+                <Stack spacing={2} direction="row">
+                    <Button variant="contained">Generate</Button>
+                    <Button variant="contained">Paste To Editor Selection</Button>
+                 </Stack>
+                 <ToggleButtonGroup
+      color="primary"
+      value={alignment}
+      exclusive
+      onChange={handleChange}
+    >
+      <ToggleButton value="web">Web</ToggleButton>
+      <ToggleButton value="android">Android</ToggleButton>
+      <ToggleButton value="ios">iOS</ToggleButton>
+    </ToggleButtonGroup>
+                <Stack direction="row" spacing={2}>
+                    <TextField
+                        id="outlined-multiline-static"
+                        label="Pattern"
+                        multiline
+                        rows={4}
+                        defaultValue="Default Value"
+                    />
+                    <TextField
+                        id="outlined-multiline-static"
+                        label="Pattern Output"
+                        multiline
+                        rows={4}
+                        defaultValue="Default Value"
+                    />
+                     
+                </Stack>
+            </Stack>
+
+            {/* <label htmlFor="pattern-input">Pattern</label>
             <textarea
                 id="pattern-input"
                 cols="30" rows="5"
@@ -61,7 +109,7 @@ const PatternGen = ({editor}) => {
                     
                     h.pasteRegexPattern(editor,geneartePattern())
                 }}>Paste To Section</button>
-            </div>
+            </div> */}
         </div>
     )
 }
