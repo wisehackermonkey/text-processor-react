713bd9dbcbfd3de28c8aaf92a485e85162610bd7
Oran C <oranbusiness@gmail.com>
2021-10-29 06:44:11 -0700

    [add] wip ui

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,6 +1,20 @@
 import * as React from 'react';
-import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import { useEffect, useState, useReducer } from "react";
 import h from "./helper"
+import Deposits from './Deposits';
+import Orders from './Orders';
+import Main from "./Main"
+import Title from './Title';
+import PatternGen from './PatternGen';
+import RegexReplace from "./RegexReplace"
+import PatternSelector from './PatternSelector';
+
+import MonacoEditor from "react-monaco-editor";
+import GitHubForkRibbon from 'react-github-fork-ribbon';
+
+import { mainListItems, secondaryListItems } from './listItems';
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+
 import CssBaseline from '@mui/material/CssBaseline';
 import MuiDrawer from '@mui/material/Drawer';
 import Box from '@mui/material/Box';
@@ -9,25 +23,15 @@ import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
 import Badge from '@mui/material/Badge';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Link from '@mui/material/Link';
+import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import NotificationsIcon from '@mui/icons-material/Notifications';
-import { mainListItems, secondaryListItems } from './listItems';
-import Deposits from './Deposits';
-import Orders from './Orders';
-import { useEffect, useState, useReducer } from "react";
-import MonacoEditor from "react-monaco-editor";
-import PatternGen from './PatternGen';
-import RegexReplace from "./RegexReplace"
-import Main from "./Main"
-import Title from './Title';
-import GitHubForkRibbon from 'react-github-fork-ribbon';
 
 function Copyright(props) {
   return (
@@ -189,6 +193,8 @@ function DashboardContent() {
                   <>
                     <h1>Regex Replace</h1>
                     <RegexReplace editor={editor} />
+                    <hr />
+                    <PatternSelector />
                     <hr />
                     <h1>Generate Text Using Regex Expressions</h1>
                     <PatternGen editor={editor} />
---
+++ src/PatternSelector.js
@@ -0,0 +1,146 @@
+import React from "react"
+import { useState } from "react"
+import { styled } from '@mui/material/styles';
+import { useTheme } from '@mui/material/styles';
+import Chip from '@mui/material/Chip';
+import Paper from '@mui/material/Paper';
+import TagFacesIcon from '@mui/icons-material/TagFaces';
+
+import Box from '@mui/material/Box';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+ 
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
+    },
+};
+
+ 
+
+const names = [
+    'Oliver Hansen',
+    'Van Henry',
+    'April Tucker',
+    'Ralph Hubbard',
+    'Omar Alexander',
+    'Carlos Abbott',
+    'Miriam Wagner',
+    'Bradley Wilkerson',
+    'Virginia Andrews',
+    'Kelly Snyder',
+];
+
+function getStyles(name, personName, theme) {
+    return {
+        fontWeight:
+            personName.indexOf(name) === -1
+                ? theme.typography.fontWeightRegular
+                : theme.typography.fontWeightMedium,
+    };
+}
+
+const ListItem = styled('li')(({ theme }) => ({
+    margin: theme.spacing(0.5),
+}));
+
+const PatternSelector = () => {
+    const theme = useTheme();
+    const [personName, setPersonName] = React.useState([]);
+
+    const handleChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setPersonName(
+            // On autofill we get a the stringified value.
+            typeof value === 'string' ? value.split(',') : value,
+        );
+    };
+    const [chipData, setChipData] = React.useState([
+        { key: 0, label: 'Angular' },
+        { key: 1, label: 'jQuery' },
+        { key: 2, label: 'Polymer' },
+        { key: 3, label: 'React' },
+        { key: 4, label: 'Vue.js' },
+    ]);
+
+    const handleDelete = (chipToDelete) => () => {
+        setChipData((chips) => chips.filter((chip) => chip.key !== chipToDelete.key));
+    };
+    return (
+        <div>
+            <Paper
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    flexWrap: 'wrap',
+                    listStyle: 'none',
+                    p: 0.5,
+                    m: 0,
+                }}
+                component="ul"
+            >
+                {chipData.map((data) => {
+                    let icon;
+
+                    if (data.label === 'React') {
+                        icon = <TagFacesIcon />;
+                    }
+
+                    return (
+                        <ListItem key={data.key}>
+                            <Chip
+                                icon={icon}
+                                label={data.label}
+                                onDelete={data.label === 'React' ? undefined : handleDelete(data)}
+                            />
+                        </ListItem>
+                    );
+                })}
+            </Paper>
+            <hr />
+            <FormControl sx={{ m: 1, width: 300 }}>
+                <InputLabel id="demo-multiple-chip-label">Chip</InputLabel>
+                <Select
+                    labelId="demo-multiple-chip-label"
+                    id="demo-multiple-chip"
+                    multiple
+                    value={personName}
+                    onChange={handleChange}
+                    input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+                    renderValue={(selected) => (
+                        <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                            {selected.map((value) => (
+                                <Chip key={value} label={value} />
+                            ))}
+                        </Box>
+                    )}
+                    MenuProps={MenuProps}
+                >
+                    {names.map((name) => (
+                        <MenuItem
+                            key={name}
+                            value={name}
+                            style={getStyles(name, personName, theme)}
+                        >
+                            {name}
+                        </MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+        </div>
+    );
+}
+
+
+
+export default PatternSelector
